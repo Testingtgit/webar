9ab39520dff38b79af559b9630aebe70edb53b8e
Testingtgit <109203890+Testingtgit@users.noreply.github.com>
2022-10-29 12:08:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const video = await loadVideo("Transavation-Final.mp4");
     const texture = new THREE.VideoTexture(video);
 
-    const geometry = new THREE.PlaneGeometry(1, 1.5);
+    const geometry = new THREE.PlaneGeometry(0.8, 1.8);
     const material = new THREE.MeshBasicMaterial({map: texture});
     const plane = new THREE.Mesh(geometry, material);
 
